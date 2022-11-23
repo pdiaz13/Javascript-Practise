5e56760c4ee48f55a0af1c4fd96e5aa988565e03
Priscila Diaz <99294646+pdiaz13@users.noreply.github.com>
2022-11-23 08:54:30 -0300

    Ejercicio seccion 09

---
+++ index.js
@@ -0,0 +1,20 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "debug",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+  ],
+});
+
+try {
+  console.log(valorX);
+} catch (e) {
+  logger.error("No se puede loggear valorX" + e);
+}
